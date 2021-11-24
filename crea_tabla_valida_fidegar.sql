3c74930300b5f9d45df38ace819e80fdf4f38a0e
Ramon <procesos5@aefcm.gob.mx>
2021-11-24 16:58:38 -0600

    validación

--- crea_tabla_valida_fidegar.sql
+++ crea_tabla_valida_fidegar.sql
@@ -6,11 +6,11 @@ CREATE TABLE PROCESA_INFORMACION.VALIDA_ALUMNOS_FIDEGAR
   ALUMNO_PRIMER_AP      VARCHAR2(60 BYTE)       NOT NULL,
   ALUMNO_SEGUNDO_AP     VARCHAR2(60 BYTE),
   CCT                   VARCHAR2(10 BYTE)       NOT NULL,
-  NIVEL                 VARCHAR2(10 BYTE)       NOT NULL,
+  NIVEL                 VARCHAR2(25 BYTE)       NOT NULL,
   GRADO                 VARCHAR2(20 BYTE),      NOT NULL,
   ALCALDIA              VARCHAR2(128 BYTE)      NOT NULL,
   CCT_AEFCM             VARCHAR2(10 BYTE),
-  NIVEL_AEFCM           VARCHAR2(15 BYTE),
+  NIVEL_AEFCM           VARCHAR2(25 BYTE),
   GRADO_AEFCM           VARCHAR2(20 BYTE),
   USUARIO_CREACION      VARCHAR2(32 BYTE)       NOT NULL,
   FECHA_CREACION        DATE                    NOT NULL,
@@ -26,3 +26,19 @@ CREATE INDEX PROCESA_INFORMACION.IDX_VALIDA_ALU_FIDEGAR_CCT ON PROCESA_INFORMACI
 CREATE UNIQUE INDEX PROCESA_INFORMACION.IDX_VALIDA_ALU_FIDEGAR_CURP ON PROCESA_INFORMACION.VALIDA_ALUMNOS_FIDEGAR
 (ALUMNO_CURP)
 ;
+
+ALTER TABLE PROCESA_INFORMACION.VALIDA_ALUMNOS_FIDEGAR MODIFY NIVEL VARCHAR(25);
+ALTER TABLE PROCESA_INFORMACION.VALIDA_ALUMNOS_FIDEGAR MODIFY NIVEL_AEFCM VARCHAR(25);
+DELETE FROM PROCESA_INFORMACION.valida_alumnos_fidegar where id > 0;
+
+select 
+sed_alumnos.sal_curp_alumno as CURP_ALUMNO,
+matricula.sed_inscripciones.sis_ses_cct_escuela as cct_aefcm,
+matricula.sed_grados_escolares.sge_descripcion_larga_grado as grado_AEFCM,
+matricula.sed_niveles_escolares.sne_desccripcion_larga_nivel as NIVEL_AEFCM
+from MATRICULA.sed_inscripciones left JOIN SED_ALUMNOS
+ on sed_alumnos.sal_curp_alumno = matricula.sed_inscripciones.sis_sal_curp_alumno
+left join MATRICULA.sed_niveles_escolares on SIS_SNE_CVE_NIVEL_ESCOLAR = SNE_CVE_NIVEL_ESCOLAR
+left join MATRICULA.sed_grados_escolares on sis_sge_cve_grado_escolar = SGE_CVE_GRADO_ESCOLAR
+where SIS_SCE_CVE_CICLO_ESCOLAR = '2021-2022' and
+matricula.sed_inscripciones.sis_sal_curp_alumno in (SELECT ALUMNO_CURP from PROCESA_INFORMACION.valida_alumnos_fidegar);
